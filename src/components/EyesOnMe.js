d5e6e14e19784d701d89641b16fa3b783b3e4835
Chun Yi <8.5by11@gmail.com>
2022-02-02 11:57:06 -0500

    commit tree f21e91da25954654d94af6c1312b0f79556c0386

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,48 @@
-// Code EyesOnMe Component Here
+//https://reactjs.org/docs/events.html#detecting-focus-entering-and-leaving
+
+// function EyesOnMe() {
+//     return (
+//         <button
+//             onFocus={(e) => {
+//                 console.log("Good!")
+//             }}
+//             >Eyes on me
+//         </button> 
+
+//     )
+// }
+
+// export default EyesOnMe;
+
+function EyesOnMe() {
+	return (
+		<button
+			onFocus={(e) => {
+				if (e.currentTarget === e.target) {
+			  		console.log('Good!');
+				} else {
+					console.log('focused child', e.target);
+				}
+				if (!e.currentTarget.contains(e.relatedTarget)) {
+					// Not triggered when swapping focus between children
+					console.log('focus entered self');
+				}
+		 	}}
+		  	onBlur={(e) => {
+				if (e.currentTarget === e.target) {
+					console.log('Hey! Eyes on me!');
+				} else {
+					console.log('unfocused child', e.target);
+				}
+				if (!e.currentTarget.contains(e.relatedTarget)) {
+					// Not triggered when swapping focus between children
+					console.log('focus left self');
+				}
+		 	}}
+		>Eyes on me
+		</button> 
+	)
+}
+
+
+export default EyesOnMe;
